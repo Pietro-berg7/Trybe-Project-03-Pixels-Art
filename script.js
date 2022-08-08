fa1b6857e6f4023409950d9bdcf915b0c7d98606
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-08-08 00:45:21 -0300

    Edita paleta de cores e quadro de pixels

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let quadroPixels = document.querySelector('#pixel-board');
-for (let i = 0; i < 5; i += 1) {
-  for (let j = 0; j < 5; j += 1) {
-    let pixel = document.createElement('div');
-    pixel.style.width = '40px';
-    pixel.style.height = '40px';
-    pixel.style.border = '1px solid black';
-    pixel.style.display = 'inline-block';
-    pixel.className = 'pixel';
-    quadroPixels.appendChild(pixel);  
+function quadroDePixels(){
+  let quadroPixels = document.querySelector('#pixel-board');
+  for (let i = 0; i < 5; i += 1) {
+    let linha = document.createElement('div');
+    for (let j = 0; j < 5; j += 1) {
+      let pixel = document.createElement('div');
+      pixel.style.width = '40px';
+      pixel.style.height = '40px';
+      pixel.style.border = '1px solid black';
+      pixel.style.display = 'inline-block';
+      pixel.className = 'pixel';
+      linha.appendChild(pixel);  
+    }
+    quadroPixels.appendChild(linha);
   }
-  let space = document.createElement('br');
-  quadroPixels.appendChild(space);
 }
+quadroDePixels();
